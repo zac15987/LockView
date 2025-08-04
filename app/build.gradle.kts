de4071d98ffd89b75574a23616bf50501ee4a6b9
zac15987 <zac15987@gmail.com>
2025-08-04 16:35:31 +0800

    chore: bump version to 1.0.1 for bug fix release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.zac15987.lockview"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
