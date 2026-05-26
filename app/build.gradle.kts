e3402eee2df1af12d41e5c0fc236f40597329b05
zac15987 <zac15987@gmail.com>
2026-05-26 16:24:42 +0800

    chore: bump version to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.zac15987.lockview"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
